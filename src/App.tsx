da85508e8b1198de44108926071ccd3703692d15
kirkwood616 <kirkwood616@gmail.com>
2022-02-07 11:38:34 -0500

    Setting up Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
-import "./components/Styles.css"
+import "./components/Styles.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import OwnerProfile from "./components/OwnerProfile";
 
 function App() {
   return (
     <div className="App">
-      <h1>APP</h1>
-      <Main />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/customer-profile" element={<OwnerProfile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
