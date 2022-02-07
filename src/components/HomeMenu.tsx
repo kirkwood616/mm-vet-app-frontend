2d7152618a4d33785979a40276880bc1d38a88e3
Momo Miller <minimomo14z@hotmail.com>
2022-02-07 10:41:29 -0500

    "initial commit"

--- src/components/HomeMenu.tsx
+++ src/components/HomeMenu.tsx
@@ -4,6 +4,15 @@ function HomeMenu() {
   return (
     <div className="HomeMenu">
       <h1>HOME MENU</h1>
+      <ul className="dropDownMenu">
+        <li> <a href="">Home</a></li>
+        <li> <a href="">About Us</a></li>
+        <li> <a href="">Services</a></li>
+        <li> <a href="">Emergency</a></li>
+        <li> <a href="">Owner Profile</a></li>
+        <li> <a href="">Pet Profile</a></li>
+        <li> <a href="">Request Appointment</a></li>
+      </ul>
     </div>
   );
 }
--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -4,6 +4,33 @@ function LogIn() {
   return (
     <div className="LogIn">
       <h1>LOGIN</h1>
+      <div className="">
+        <div className="">
+          <img src="" alt="Logo" />
+          <h2>Sign In</h2>
+          <form action="" method="post" className="form">
+
+            <div className="form_field">
+              <input type="email" placeholder="info@email.com" />
+            </div>
+
+              <div className="form_field">
+                <input type="password" placeholder="••••••••••••" />
+              </div>
+
+              <div className="form_field">
+                <input type="submit" value="Sign In" />
+              </div>
+
+          </form>
+
+          <p>
+            Don't have an account?
+            <a href="#">Create an account</a>
+          </p>
+
+        </div>
+      </div>
     </div>
   );
 }
