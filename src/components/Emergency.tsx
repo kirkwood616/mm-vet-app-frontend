577abe67e15d30d8f02134c373d9087caab4107b
Momo Miller <minimomo14z@hotmail.com>
2022-02-07 16:49:28 -0500

    basic layout on components

--- src/components/Emergency.tsx
+++ src/components/Emergency.tsx
@@ -3,7 +3,16 @@
 function Emergency() {
   return (
     <div className="Emergency">
-      <h1>EMERGENCY</h1>
+      <h1>EMERGENCY INFORMATION</h1>
+      <p>
+        If you have an emergency after our regular business hours please
+        contact:
+      </p>
+      <li>
+        <a href="https://www.google.com/maps/search/emergency+animal+hospital+detroit/@42.437424,-83.3964379,10z/data=!3m1!4b1">Emergency animal hospital near Detroit</a>
+      </li>
+
+      <button><a href="/homepage">Back to homepage</a></button>
     </div>
   );
 }
--- src/components/RequestAppointment.tsx
+++ src/components/RequestAppointment.tsx
@@ -4,6 +4,30 @@ function RequestAppointment() {
   return (
     <div className="RequestAppointment">
       <h1>REQUEST APPOINTMENT</h1>
+      <form action="" className="form-container">
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="pet-name">Pet Name:</label>
+        {/* enter pet name or select dropdown  */}
+        <input type="text" name="pet-name" id="pet-name" />
+        {/* dropDownPet */}
+        <select name="pet-name" id="pet-name">
+          {}
+        </select>
+        <br />
+        <label htmlFor="calendar">Date:</label>
+        <input
+          type="date"
+          placeholder="MM/DD/YYYY"
+          data-role="calendarpicker"
+        ></input>
+        <label htmlFor="time">Time:</label>
+        <input type="time" placeholder="HH:MM AM/PM" />
+        <label htmlFor="note">Reason for Visit</label>
+        <textarea name="note" id="note"></textarea>
+        <br />
+        <button type="submit">Submit your Request</button>
+      </form>
     </div>
   );
 }
