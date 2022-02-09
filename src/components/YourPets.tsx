f6fce0235b06f31cb6d668b5d0f85d753bad870c
kirkwood616 <kirkwood616@gmail.com>
2022-02-09 14:06:16 -0500

    Added routing & link to pet-profile

--- src/components/YourPets.tsx
+++ src/components/YourPets.tsx
@@ -1,6 +1,7 @@
 import "./YourPets.css";
 import { useContext } from "react";
 import UserContext from "../context/UserContext";
+import { Link } from "react-router-dom";
 
 function YourPets() {
   let { userPets } = useContext(UserContext);
@@ -9,15 +10,17 @@ function YourPets() {
   return (
     <div className="YourPets">
       <h1>YOUR PETS</h1>
-      {userPets.map((pet) => (
-        <div className="yourPetBar">
-          <img
-            src="https://www.freeiconspng.com/uploads/png-file-png-file-png-file-png-file-png-file-30.png"
-            width="100"
-            alt="paw icon"
-          />
-          <h2>{pet.petFirstName}</h2>
-        </div>
+      {userPets.map((pet, index) => (
+        <Link to={`/pet-profile/${index}`}>
+          <div className="yourPetBar" key={index}>
+            <img
+              src="https://www.freeiconspng.com/uploads/png-file-png-file-png-file-png-file-png-file-30.png"
+              width="100"
+              alt="paw icon"
+            />
+            <h2>{pet.petFirstName}</h2>
+          </div>
+        </Link>
       ))}
     </div>
   );
