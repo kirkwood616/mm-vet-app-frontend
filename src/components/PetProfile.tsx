5c47c1d649c5be633669622bd7b5f93a2b21e657
kirkwood616 <kirkwood616@gmail.com>
2022-02-09 14:34:27 -0500

    Setting up custom pet profile

--- src/components/PetProfile.tsx
+++ src/components/PetProfile.tsx
@@ -1,9 +1,20 @@
 // import "./PetProfile.css";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+import { useParams } from "react-router-dom";
 
 function PetProfile() {
+  let { userPets } = useContext(UserContext);
+  let { index } = useParams();
+
   return (
     <div className="PetProfile">
-      <h1>PET PROFILE</h1>
+      <h1>{userPets[Number(index)].petFirstName}'s Profile</h1>
+      <img
+        src="https://www.freeiconspng.com/uploads/png-file-png-file-png-file-png-file-png-file-30.png"
+        width="150"
+        alt="paw icon"
+      />
     </div>
   );
 }
