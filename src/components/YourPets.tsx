79aa177fd738c343ce569990ac84441378c09ba1
Momo Miller <minimomo14z@hotmail.com>
2022-02-09 09:21:06 -0500

    adding basic info

--- src/components/YourPets.tsx
+++ src/components/YourPets.tsx
@@ -4,6 +4,10 @@ function YourPets() {
   return (
     <div className="YourPets">
       <h1>YOUR PETS</h1>
+      <div className="pet-card">
+     <a href="/pet-profile"> <img src="" alt="" height={300} width={300} />Pet Name</a>
+      </div>
+
     </div>
   );
 }
