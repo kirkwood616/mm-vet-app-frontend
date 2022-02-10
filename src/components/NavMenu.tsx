8c80fdc26b2e4c648224dcfda5c5f878f075fdf1
kirkwood616 <kirkwood616@gmail.com>
2022-02-10 09:39:07 -0500

    NavMenu build

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,39 +1,67 @@
+import React, { useEffect, useRef, useState } from "react";
 import "./NavMenu.css";
+import { Link } from "react-router-dom";
 
 function NavMenu() {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false);
+  const onClick = () => setIsActive(!isActive);
+
+  useEffect(() => {
+    const pageClickEvent = (e: any): void => {
+      if (dropdownRef.current !== null) {
+        setIsActive(!isActive);
+      }
+    };
+
+    if (isActive) {
+      window.addEventListener("click", pageClickEvent);
+    }
+
+    return () => {
+      window.removeEventListener("click", pageClickEvent);
+    };
+  }, [isActive]);
+
   return (
-    <div className="HomeMenu">
-      {/* top nav for mobile/ tablet */}
-      <ul className="dropDownMenu">
-        <li>
-          {" "}
-          <a href="">Home</a>
-        </li>
-        <li>
-          {" "}
-          <a href="">About Us</a>
-        </li>
-        <li>
-          {" "}
-          <a href="">Services</a>
-        </li>
-        <li>
-          {" "}
-          <a href="">Emergency</a>
-        </li>
-        <li>
-          {" "}
-          <a href="">Owner Profile</a>
-        </li>
-        <li>
-          {" "}
-          <a href="">Pet Profile</a>
-        </li>
-        <li>
-          {" "}
-          <a href="">Request Appointment</a>
-        </li>
-      </ul>
+    <div className="NavMenu">
+      <div className="navContainer">
+        <button onClick={onClick} className="menuTrigger">
+          <span>Menu</span>
+          <img src="hamburger_icon.svg" alt="" className="hamburger" />
+        </button>
+        <nav
+          ref={dropdownRef}
+          className={`menu ${isActive ? "active" : "inactive"}`}
+        >
+          <ul>
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/user-profile"}>Your Profile</Link>
+            </li>
+            <li>
+              <Link to={"/your-pets"}>Your Pets</Link>
+            </li>
+            <li>
+              <Link to={"/request-appointment"}>Request Appointment</Link>
+            </li>
+            <li>
+              <Link to={"/emergency"}>Emergency</Link>
+            </li>
+            <li>
+              <Link to={"/services"}>Services</Link>
+            </li>
+            <li>
+              <Link to={"/about-us"}>About Us</Link>
+            </li>
+            <li>
+              <Link to={"/"}>Logout</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
