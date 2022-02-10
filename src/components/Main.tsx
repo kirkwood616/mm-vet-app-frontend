136a6c26a248ea4cf5d81acea2df5600ce34606f
kirkwood616 <kirkwood616@gmail.com>
2022-02-09 22:07:47 -0500

    Removed Footer component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,4 @@
 import "./Main.css";
-import Footer from "./Footer";
 import HomeMenu from "./HomeMenu";
 import LogIn from "./LogIn";
 import UserContext from "../context/UserContext";
@@ -8,12 +7,7 @@ import { useContext } from "react";
 function Main() {
   let { isLoggedIn } = useContext(UserContext);
 
-  return (
-    <div className="Main">
-      {isLoggedIn ? <HomeMenu /> : <LogIn />}
-      <Footer />
-    </div>
-  );
+  return <div className="Main">{isLoggedIn ? <HomeMenu /> : <LogIn />}</div>;
 }
 
 export default Main;
