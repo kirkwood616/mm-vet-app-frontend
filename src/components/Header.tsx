e64634dcb5d47421542510d7d1f673b1c92ae530
kirkwood616 <kirkwood616@gmail.com>
2022-02-10 09:47:56 -0500

    Changed logo img styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,11 @@ import NavMenu from "./NavMenu";
 function Header() {
   return (
     <div className="Header">
-      <div className="">
-        <img
-          className="headerLogo"
-          src="M&M veterinary clinic.png"
-          alt="M&M Veterinary Clinic Top Logo"
-          height={150}
-        />
-      </div>
+      <img
+        className="headerLogo"
+        src="M&M veterinary clinic.png"
+        alt="M and M Veterinary Clinic Top Logo"
+      />
       <NavMenu />
     </div>
   );
