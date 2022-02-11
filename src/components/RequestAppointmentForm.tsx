836e09e3ced61a26ccb42f2b25348e788cd2b654
kirkwood616 <kirkwood616@gmail.com>
2022-02-11 14:57:05 -0500

    Configuring & styling Req Form

--- src/components/RequestAppointmentForm.tsx
+++ src/components/RequestAppointmentForm.tsx
@@ -1,24 +1,39 @@
 import "./RequestAptForm.css";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 function RequestAptForm() {
+  let { user } = useContext(UserContext);
+  let { userPets } = useContext(UserContext);
+
   return (
     <div className="RequestAptForm">
       <form action="submit" className="formContainer">
         <label htmlFor="name">
           <p>Name:</p>
         </label>
-        <input type="text" name="name" id="name" />
+        <input
+          readOnly
+          type="text"
+          name="name"
+          id="name"
+          defaultValue={`${user.firstName} ${user.lastName}`}
+        />
         <label htmlFor="pet-name">
           <p>Pet Name:</p>
         </label>
-        {/* enter pet name or select dropdown  */}
-        <input type="text" name="pet-name" id="pet-name" />
-        {/* dropDownPet */}
-        <select name="pet-name" id="pet-name">
-          {}
+        {/* <input type="text" name="pet-name" id="pet-name" /> */}
+        <select name="pet-name" id="pet-name" defaultValue={"select"} required>
+          <option value={"select"} disabled>
+            --- Select Pet ---
+          </option>
+          {userPets.map((pet, index) => (
+            <option value={pet.petFirstName} key={index}>
+              {pet.petFirstName}
+            </option>
+          ))}
         </select>
-        <br />
         <label htmlFor="calendar">
           <p>Date:</p>
         </label>
@@ -26,18 +41,26 @@ function RequestAptForm() {
           type="date"
           placeholder="MM/DD/YYYY"
           data-role="calendarpicker"
+          id="calendar"
+          required
         ></input>
         <label htmlFor="time">
           <p>Time:</p>
         </label>
-        <input type="time" placeholder="HH:MM AM/PM" />
+        <input
+          type="time"
+          placeholder="HH:MM AM/PM"
+          id="time"
+          min={"08:00"}
+          max={"18:00"}
+          required
+        />
         <label htmlFor="note">
           <p>Reason For Request:</p>
         </label>
-        <textarea name="note" id="note"></textarea>
-        <br />
+        <textarea name="note" id="note" required></textarea>
         <Link to={"/request-confirmation"}>
-          <button>Request Appointment</button>
+          <button>Send Request</button>
         </Link>
       </form>
     </div>
