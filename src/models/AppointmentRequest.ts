a3ad7feccdb1817f89eeb3b5c1aa583b6fa6f2e2
kirkwood616 <kirkwood616@gmail.com>
2022-02-16 11:27:44 -0500

    Added Apt Request model

---
+++ src/models/AppointmentRequest.ts
@@ -0,0 +1,10 @@
+interface AppointmentRequest {
+  _id?: string;
+  userName: string;
+  userPet: string;
+  requestDate: string;
+  requestTime: string;
+  requestReason: string;
+}
+
+export default AppointmentRequest;
