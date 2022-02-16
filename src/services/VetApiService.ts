31671f0e9ff11c2228eb004d8bd04c3be41b2579
kirkwood616 <kirkwood616@gmail.com>
2022-02-15 20:30:53 -0500

    Added put & delete + formatting

--- src/services/VetApiService.ts
+++ src/services/VetApiService.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 import User from "../models/User";
 import Pet from "../models/Pet";
 import MessageBoardPost from "../models/MessageBoardPost";
-// import MedicalRecord from "../models/MedicalRecord";
 
-// CUSTOMERS
+/////////////// CUSTOMERS ///////////////
 export function fetchAllUsers(): Promise<User[]> {
   return axios
     .get("http://localhost:5001/mm-vet-app/us-central1/api/users")
@@ -23,33 +22,32 @@ export function fetchUserByEmail(email: string): Promise<User> {
     .then((res) => res.data);
 }
 
-// PETS
+/////////////// PETS ///////////////
+
+// GET (ALL)
 export function fetchAllPets(): Promise<Pet[]> {
   return axios
     .get("http://localhost:5001/mm-vet-app/us-central1/api/pets")
     .then((res) => res.data);
 }
 
+// GET (ONE)
 export function fetchPet(id: string): Promise<Pet> {
   return axios
     .get(`http://localhost:5001/mm-vet-app/us-central1/api/pet/${id}`)
     .then((res) => res.data);
 }
 
+// PUT
 export function updatePet(id: string, pet: Pet): Promise<Pet> {
   return axios
     .put(`http://localhost:5001/mm-vet-app/us-central1/api/pet/${id}`, pet)
     .then((res) => res.data);
 }
 
-// MEDICAL RECORDS
-// export function fetchAllMedicalRecords(): Promise<MedicalRecord[]> {
-//   return axios
-//     .get("http://localhost:5001/mm-vet-app/us-central1/api/medical-records")
-//     .then((res) => res.data);
-// }
+/////////////// MESSAGE BOARD ///////////////
 
-// MESSAGE BOARD
+// GET
 export function fetchGeneralPosts(): Promise<MessageBoardPost[]> {
   return axios
     .get(
@@ -58,11 +56,10 @@ export function fetchGeneralPosts(): Promise<MessageBoardPost[]> {
     .then((res) => res.data);
 }
 
+// POST
 export function postMessageToBoard(
   post: MessageBoardPost
 ): Promise<MessageBoardPost> {
-  console.log("IN SERVICE!");
-
   return axios
     .post<MessageBoardPost>(
       "http://localhost:5001/mm-vet-app/us-central1/api/message-board",
@@ -71,13 +68,24 @@ export function postMessageToBoard(
     .then((res) => res.data);
 }
 
-// export function sendGeneralPost(
-//   post: MessageBoardPost
-// ): Promise<MessageBoardPost> {
-//   return axios
-//     .post<MessageBoardPost>(
-//       `http://localhost:5001/mm-vet-app/us-central1/api/message-board/general`,
-//       post
-//     )
-//     .then((res) => res.data);
-// }
+// PUT
+export function updateMessageFromBoard(
+  id: string,
+  postEdit: MessageBoardPost
+): Promise<MessageBoardPost> {
+  return axios
+    .put<MessageBoardPost>(
+      `http://localhost:5001/mm-vet-app/us-central1/api/message-board/${id}`,
+      postEdit
+    )
+    .then((res) => res.data);
+}
+
+// DELETE
+export function deleteMessageFromBoard(id: string): Promise<MessageBoardPost> {
+  return axios
+    .delete<MessageBoardPost>(
+      `http://localhost:5001/mm-vet-app/us-central1/api/message-board/${id}`
+    )
+    .then((res) => res.data);
+}
