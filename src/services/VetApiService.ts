792167ac9a5d4cb302365b6a9675c9767dd600bf
kirkwood616 <kirkwood616@gmail.com>
2022-02-16 16:08:19 -0500

    Added live routing

--- src/services/VetApiService.ts
+++ src/services/VetApiService.ts
@@ -7,19 +7,19 @@ import AppointmentRequest from "../models/AppointmentRequest";
 /////////////// CUSTOMERS ///////////////
 export function fetchAllUsers(): Promise<User[]> {
   return axios
-    .get("http://localhost:5001/mm-vet-app/us-central1/api/users")
+    .get("https://us-central1-mm-vet-app.cloudfunctions.net/api/users")
     .then((res) => res.data);
 }
 
 export function fetchUser(id: string): Promise<User> {
   return axios
-    .get(`http://localhost:5001/mm-vet-app/us-central1/api/users/${id}`)
+    .get(`https://us-central1-mm-vet-app.cloudfunctions.net/api/users/${id}`)
     .then((res) => res.data);
 }
 
 export function fetchUserByEmail(email: string): Promise<User> {
   return axios
-    .get(`http://localhost:5001/mm-vet-app/us-central1/api/user/${email}`)
+    .get(`https://us-central1-mm-vet-app.cloudfunctions.net/api/user/${email}`)
     .then((res) => res.data);
 }
 
@@ -28,21 +28,21 @@ export function fetchUserByEmail(email: string): Promise<User> {
 // GET (ALL)
 export function fetchAllPets(): Promise<Pet[]> {
   return axios
-    .get("http://localhost:5001/mm-vet-app/us-central1/api/pets")
+    .get("https://us-central1-mm-vet-app.cloudfunctions.net/api/pets")
     .then((res) => res.data);
 }
 
 // GET (ONE)
 export function fetchPet(id: string): Promise<Pet> {
   return axios
-    .get(`http://localhost:5001/mm-vet-app/us-central1/api/pet/${id}`)
+    .get(`https://us-central1-mm-vet-app.cloudfunctions.net/api/pet/${id}`)
     .then((res) => res.data);
 }
 
 // PUT
 export function updatePet(id: string, pet: Pet): Promise<Pet> {
   return axios
-    .put(`http://localhost:5001/mm-vet-app/us-central1/api/pet/${id}`, pet)
+    .put(`https://us-central1-mm-vet-app.cloudfunctions.net/api/pet/${id}`, pet)
     .then((res) => res.data);
 }
 
@@ -53,7 +53,7 @@ export function sendAppointmentRequest(
 ): Promise<Pet> {
   return axios
     .post(
-      `http://localhost:5001/mm-vet-app/us-central1/api/appointment-requests/`,
+      `https://us-central1-mm-vet-app.cloudfunctions.net/api/appointment-requests/`,
       request
     )
     .then((res) => res.data);
@@ -65,7 +65,7 @@ export function sendAppointmentRequest(
 export function fetchGeneralPosts(): Promise<MessageBoardPost[]> {
   return axios
     .get(
-      `http://localhost:5001/mm-vet-app/us-central1/api/message-board/general`
+      `https://us-central1-mm-vet-app.cloudfunctions.net/api/message-board/general`
     )
     .then((res) => res.data);
 }
@@ -76,7 +76,7 @@ export function postMessageToBoard(
 ): Promise<MessageBoardPost> {
   return axios
     .post<MessageBoardPost>(
-      "http://localhost:5001/mm-vet-app/us-central1/api/message-board",
+      "https://us-central1-mm-vet-app.cloudfunctions.net/api/message-board",
       post
     )
     .then((res) => res.data);
@@ -89,7 +89,7 @@ export function updateMessageFromBoard(
 ): Promise<MessageBoardPost> {
   return axios
     .put<MessageBoardPost>(
-      `http://localhost:5001/mm-vet-app/us-central1/api/message-board/${id}`,
+      `https://us-central1-mm-vet-app.cloudfunctions.net/api/message-board/${id}`,
       postEdit
     )
     .then((res) => res.data);
@@ -99,7 +99,7 @@ export function updateMessageFromBoard(
 export function deleteMessageFromBoard(id: string): Promise<MessageBoardPost> {
   return axios
     .delete<MessageBoardPost>(
-      `http://localhost:5001/mm-vet-app/us-central1/api/message-board/${id}`
+      `https://us-central1-mm-vet-app.cloudfunctions.net/api/message-board/${id}`
     )
     .then((res) => res.data);
 }
