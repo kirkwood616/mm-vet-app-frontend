09b4dab3437420ad25ff557fa03f94c9fed2211c
kirkwood616 <kirkwood616@gmail.com>
2022-02-16 00:11:22 -0500

    Changed layout to row

--- src/components/YourPets.tsx
+++ src/components/YourPets.tsx
@@ -9,18 +9,20 @@ function YourPets() {
   return (
     <div className="YourPets">
       <h1>YOUR PETS</h1>
-      {userPets.map((pet, index) => (
-        <Link to={`/pet-profile/${pet._id}`} key={index}>
-          <div className="yourPetBar">
-            <img
-              src={userPets[index].picture.picture}
-              alt={`${userPets[index].petFirstName}`}
-              className="userPetPic"
-            />
-            <h2>{pet.petFirstName}</h2>
-          </div>
-        </Link>
-      ))}
+      <div className="yourPetsContainer">
+        {userPets.map((pet, index) => (
+          <Link to={`/pet-profile/${pet._id}`} key={index}>
+            <div className="yourPetBar">
+              <img
+                src={userPets[index].picture.picture}
+                alt={`${userPets[index].petFirstName}`}
+                className="userPetPic"
+              />
+              <h2>{pet.petFirstName}</h2>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
