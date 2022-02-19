6a8a935f3d55c4200f5d37f86b4badda91bc7e89
kirkwood616 <kirkwood616@gmail.com>
2022-02-19 18:21:20 -0500

    Refactored layout

--- src/components/Emergency.tsx
+++ src/components/Emergency.tsx
@@ -6,35 +6,36 @@ function Emergency() {
     <div className="Emergency">
       <h1>EMERGENCY INFORMATION</h1>
       <div className="emergencyInfo">
-        <img className="emergencyimage" src="./animal-photos/er.jpg" alt="" />
-       <br />
+        <img className="emergencyImage" src="./animal-photos/er.jpg" alt="" />
+        <br />
         <p>
           If you have an emergency after our regular business hours please
           contact:
         </p>
-        
+        <p>
+          <strong>Animal Poison Control Center</strong>
+        </p>
+        <li>(888) 426-4435</li>
         <li>
-          Animal Poison Control Center 
-          at (888)426-4435 | (888) 426-4435.{" "}
-          <br />
           <a
             href="https://www.aspca.org/pet-care/animal-poison-control"
             target="_blank"
             rel="noopener noreferrer"
           >
-            ASPCA Click here
+            ASPCA Website
           </a>
         </li>
-        
-        <li>
-          <a
-            href="https://www.google.com/maps/search/emergency+animal+hospital+detroit/@42.4367417,-83.3964396,10z/data=!4m3!2m2!5m1!2e3"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            24 Hour Emergency Animal Hospitals Near Detroit
-          </a>
-        </li>
+        <p>
+          <strong>
+            <a
+              href="https://www.google.com/maps/search/emergency+animal+hospital+detroit/@42.4367417,-83.3964396,10z/data=!4m3!2m2!5m1!2e3"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              24 Hour Emergency Animal Hospitals Near Detroit
+            </a>
+          </strong>
+        </p>
       </div>
       <br />
       <Link to={"/"}>
