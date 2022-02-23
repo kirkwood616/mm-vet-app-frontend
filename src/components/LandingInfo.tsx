f1871a953e4ae7268dafc856f8c7ffe70ff8937f
kirkwood616 <kirkwood616@gmail.com>
2022-02-23 15:00:05 -0500

    Media query, styling, refactor

--- src/components/LandingInfo.tsx
+++ src/components/LandingInfo.tsx
@@ -9,16 +9,12 @@ function LandingInfo() {
       onClick={() => setHide(true)}
     >
       <div className="infoBackground">
-        <h1>WELCOME!</h1>
-        <br />
+        <h2>Welcome!</h2>
         <p>Password Authentication is not enabled at this time.</p>
-        <br />
         <p>To view & test this app, use the following e-mail log-ins:</p>
-        <br />
         <p className="login">bill@email.com</p>
         <p className="login">grantchirpus@email.com</p>
         <p className="login">sally@email.com</p>
-        <p className="click">CLICK TO CLOSE WINDOW</p>
       </div>
     </div>
   );
